761d18c4eb1779b1b06875c279f8b77edc93b0aa
blancasserna <44443477+blancasserna@users.noreply.github.com>
2018-10-25 11:31:35 +0200

    Create aprovarm03.py

---
+++ aprovarm03.py
@@ -0,0 +1,7 @@
+abduccion = input ("Indica si te han abducido:")
+jamon = input ("Indica si tienes un jamon:")
+estudiar = input ("Indica si has estudiado:")
+if ((jamon == "si") or (estudiar == "mucho") or (abduccion == "si")) : 
+  print ("Has aprovado M03")
+else:
+  print ("No has aprovado")
