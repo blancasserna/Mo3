be55ec03a5dcaae584e4b04ef8f8d8dd6f37b997
blancasserna <44443477+blancasserna@users.noreply.github.com>
2018-10-28 13:12:28 +0100

    Create jubilats.py

---
+++ python/jubilats.py
@@ -0,0 +1,15 @@
+jubilat = input("Indica si estas jubilat/da:")
+sexe = input("Indica el teu sexe:")
+cabell = input("Indica el teu color de cabell:")
+if (sexe == "home") and (jubilat == "si") :
+  print ("Pagues 1 euro")
+else:
+  print ("No pagues 1 euro")
+if (jubilat == "no") and (sexe == "dona") and (cabell == "ros") :
+  print ("No pagues")
+else:
+  print ("Si pagues")
+if (jubilat == "si) :
+  print ("Gratis")
+else:
+  print ("Pagues")
