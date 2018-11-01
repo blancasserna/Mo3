dbda12a81d5ab06f9b41b3c13d243ac01cb7b088
blancasserna <44443477+blancasserna@users.noreply.github.com>
2018-11-01 13:08:21 +0100

    Update edad3.py

--- python/edad3.py
+++ python/edad3.py
@@ -1,9 +1,7 @@
 edat = int(input("Indica l'edat:"))
 if ((edat >= 18) and (edat <= 23)) :
   print ("Pots entrar en la sessió de joves")
-else:
-  print ("No pots entrar en la sessió de joves")
 if (edat == 17) :
     print ("Pots entrar en la sessió de joves")
 else:
-    print ("No pots entrar en la sessió de joves")
+  print ("No pots entrar en la sessió de joves")
