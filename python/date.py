1c170607798ead568b28c4e594198129679af3dd
blancasserna <44443477+blancasserna@users.noreply.github.com>
2018-11-07 16:34:29 +0100

    Update date.py

--- python/date.py
+++ python/date.py
@@ -6,9 +6,8 @@ if (any_actual == any_qualsevol):
 else:
     res = (any_actual) - (any_qualsevol)
     if (any_actual > any_qualsevol):
-        print("Desde l'any,"any_qualsevol",han passat,"res",anys!")
+        print("Desde l'any", any_qualsevol,"han passat", res,"anys!")
     else:
         res = (any_qualsevol) - (any_actual)
         if (any_qualsevol > any_actual):
-            print("Per arrivar a l'any,"any_qualsevol",falten,"res",anys!")
-                    
+            print("Per arrivar a l'any", any_qualsevol,"falten", res,"anys!")
