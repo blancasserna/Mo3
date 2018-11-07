8128222c71effb84db0792c430659531e26218c0
blancasserna <44443477+blancasserna@users.noreply.github.com>
2018-11-07 16:30:36 +0100

    Update date.py

--- python/date.py
+++ python/date.py
@@ -1,1 +1,14 @@
-
+# coding: utf-8
+any_actual = int(input("Indica l'any actual:")
+any_qualsevol = int(input("Indica un any qualsevol:")
+if (any_actual == any_qualsevol):
+    print("És el mateix any!")
+else:
+    res = (any_actual) - (any_qualsevol)
+    if (any_actual > any_qualsevol):
+        print("Desde l'any,"any_qualsevol",han passat,"res",anys!")
+    else:
+        res = (any_qualsevol) - (any_actual)
+        if (any_qualsevol > any_actual):
+            print("Per arrivar a l'any,"any_qualsevol",falten,"res",anys!")
+                    
