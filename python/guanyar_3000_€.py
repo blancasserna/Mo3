19d3ba5790a7e3cc13c409ed22d1942157450930
blancasserna <44443477+blancasserna@users.noreply.github.com>
2018-11-07 16:02:29 +0100

    Create guanyar_3000_€.py

---
+++ python/guanyar_3000_€.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+edat = int(input("Indique su edad:"))
+casat = input("Indica si estas casat/a:")
+loteria = input("Indica si te ha tocado la loteria:")
+problema_cor = input("Indica si tens problemes de cor:")
+nota_MO1 = int(input("Indica la teva nota del Mo1:"))
+nota_MO2 = int(input("Indica la teva nota del Mo2:"))
+nota_MO3 = int(input("Indica la teva nota del Mo3:"))
+nota_MO5 = int(input("Indica la teva nota del Mo5:"))
+mitja = (nota_MO1*0.3)+(nota_MO2*0.4)+(nota_MO3*0.25)+(nota_MO5*0.05)
+if (loteria == "si"):
+	print("Guanyaré 3000 euros al mes")
+if (edat >= 80) and (problemas_cor == "si") and (casat == "si")
+if (mitja >= 8):
+	print("Guanyaré 3000 euros al mes")
+else:
+	print("No guanyaré 3000 euros al mes")
