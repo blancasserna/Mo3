ab4d6a6de759a8397866a762cbae5a5a270a07eb
blancasserna <44443477+blancasserna@users.noreply.github.com>
2018-11-08 11:49:17 +0100

    Update programa_nombre3.py

--- python/programa_nombre3.py
+++ python/programa_nombre3.py
@@ -1,1 +1,18 @@
+#coding: utf-8
+"""Joc de proves:
+Entrada     Sortida
+2  3        2  3
+3  2        2  3
+8 -6        -6 8
+2  2        2  2 """
 
+nombre1 = int(input("Indica un nombre:"))
+nombre2 = int(input("Indica un altre nombre:"))
+if (nombre1 < nombre2):
+    print(nombre1,nombre2)
+else:
+    if (nombre1 > nombre2)
+        print(nombre1,nombre2)
+    else:
+        if (nombre1 = nombre2):
+            print(nombre1,nombre2)
