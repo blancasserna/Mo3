3f9da47194522e95bca89559127fecf5a58df3a7
blancasserna <44443477+blancasserna@users.noreply.github.com>
2018-11-08 10:03:11 +0100

    Update numero.py

--- python/numero.py
+++ python/numero.py
@@ -1,1 +1,7 @@
-
+#coding: utf-8
+numero = int(input("Indica un número qualsevol:"))
+if (numero >= 0):
+    print("És un número positiu.")
+else:
+    if (numero < 0):
+        print("És un número negatiu.")
