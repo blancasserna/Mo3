0b0398d518529cedc84f02816dd15f5c77ebe471
blancasserna <44443477+blancasserna@users.noreply.github.com>
2018-11-08 10:23:03 +0100

    Update programa_positiu1.py

--- python/programa_positiu1.py
+++ python/programa_positiu1.py
@@ -1,4 +1,9 @@
 #coding: utf-8
+"""Joc de proves
+Entrada     Sortida
+3           positiu
+-2          no positiu
+0           positiu"""
 numero = int(input("Indica un número qualsevol:"))
 if (numero >= 0):
     print("És un número positiu.")
