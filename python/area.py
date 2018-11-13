0668773bec360e6aecedd497ac7d7605506e3046
blancasserna <44443477+blancasserna@users.noreply.github.com>
2018-11-13 16:14:51 +0100

    Update area.py

--- python/area.py
+++ python/area.py
@@ -4,9 +4,15 @@ if ( lletra == "T" ):
     base = float(input("Indica la base del triangle:"))
     altura = float(input("Indica l'alçada del triangle:"))
     area_triangle = (base * altura)/2
-    print("L'àrea del triangle és" ,area_triangle)
+    if ( base == 0 or altura == 0 ):
+        print("La base i l'alçada no poden ser 0.")
+    else:
+        print("L'àrea del triangle és" ,area_triangle)
 else:
     if ( lletra == "C"):
         radi = float(input("Indica el radi del cercle:"))
         area_cercle = (radi * radi * 3.1415)
-        print("L'àrea del cercle és" ,area_cercle)
+        if ( radi == 0 ):
+            print("El radi no pot ser 0.") 
+        else:
+            print("L'àrea del cercle és" ,area_cercle)
