fdd3ca7a190d56281c0e1ff4a456d45ab1f3eea5
blancasserna <44443477+blancasserna@users.noreply.github.com>
2018-11-13 15:29:19 +0100

    Update area.py

--- python/area.py
+++ python/area.py
@@ -1,13 +1,12 @@
 #coding: utf-8
 lletra = input("Indica de quina figura vols calcular l'àrea:")
 if ( lletra == "T" ):
-    base = int(input("Indica la base del triangle:"))
-    altura = int(input("Indica l'alçada del triangle:"))
+    base = float(input("Indica la base del triangle:"))
+    altura = float(input("Indica l'alçada del triangle:"))
     area_triangle = (base * altura)/2
     print("L'àrea del triangle és" ,area_triangle)
 else:
     if ( lletra == "C"):
-        radi = int(input("Indica el radi del cercle:"))
+        radi = float(input("Indica el radi del cercle:"))
         area_cercle = (radi * radi * 3.1415)
         print("L'àrea del cercle és" ,area_cercle)
-    
