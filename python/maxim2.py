432bc4aa9f2271d73ddfaaf4a345b0446bdb4eef
blancasserna <44443477+blancasserna@users.noreply.github.com>
2018-11-15 10:35:51 +0100

    Update maxim2.py

--- python/maxim2.py
+++ python/maxim2.py
@@ -1,1 +1,11 @@
-
+#coding: utf-8
+num1 = int(input("Indica un nombre:"))
+num2 = int(input("Indica un segon nombre:"))
+num3 = int(input("Indica un tercer nombre:"))
+if (num1 < num2 and num3 < num2):
+    print(num2)
+else:
+    if(num1 < num3 and num2 < num3):
+        print(num3)
+    else:
+        print(num1)        
