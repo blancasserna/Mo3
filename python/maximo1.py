63987ad0efbfe856c1760fc176999f0d9dd90011
blancasserna <44443477+blancasserna@users.noreply.github.com>
2018-11-15 10:15:22 +0100

    Update and rename llegirnombres.py to maximo1.py

---
+++ python/maximo1.py
@@ -0,0 +1,8 @@
+#coding: utf-8
+num1 = int(input("Indica un nombre:"))
+num2 = int(input("Indica un altre nombre:"))
+if (num1 < num2):
+    print(num2)
+else:
+    if (num1 > num2):
+        print(num1)
