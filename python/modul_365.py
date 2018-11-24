c42983e1e7edc20e06bae99897ff3282952c9e85
blancasserna <44443477+blancasserna@users.noreply.github.com>
2018-11-24 11:08:56 +0100

    Update and rename modul.py to modul_365.py

--- python/modul_365.py
+++ python/modul_365.py
@@ -1,7 +1,6 @@
 # coding:utf-8
 
 for num_dia in range(0, 365):
-    print("Volta nº,num_dia + 1)
+    print("Volta nº" , num_dia + 1)
     if (num_dia % 365) == (350-1):
         print("*")
-        
