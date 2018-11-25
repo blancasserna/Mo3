0f9b197046689b493b309ede5d13b5120eb204b3
blancasserna <44443477+blancasserna@users.noreply.github.com>
2018-11-25 17:59:25 +0100

    Update modul_100.py

--- python/modul_100.py
+++ python/modul_100.py
@@ -1,1 +1,16 @@
+# coding:utf-8
 
+for primer_rang in range(0, 100):
+    print("Volta nº" , primer_rang + 1)
+    print("Premeu qualsevol tecla per continuar")
+    for segon_rang in range(100, 200):
+        print("Volta nº" , segon_rang + 1)
+        print("Premeu qualsevol tecla per continuar")
+        for tercer_rang in range(200, 300):
+            print("Volta nº" , tercer_rang + 1)
+            print("Premeu qualsevol tecla per continuar")
+            for quart_rang in range(300, 365):
+                print("Volta nº" , quart_rang + 1)
+                print("Premeu qualsevol tecla per continuar")
+    if (num_dia % 365) == (350-1):
+        print("*")
