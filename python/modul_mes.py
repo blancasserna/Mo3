e1a96d1244d17e7d0fda3dbe8982f06fbdf83e22
blancasserna <44443477+blancasserna@users.noreply.github.com>
2018-11-26 15:45:36 +0100

    Delete modul_mes.py

--- python/modul_mes.py
+++
@@ -1,4 +0,0 @@
-for mes_de_lany in list ("gener, febrer, marc, abril, maig, juny, juliol, agost, septembre, octubre, novembre, desembre" == 12):
-    print("Mes:" , mes_de_lany)
-    if ("mes_de_lany" % 12) == (4-1):
-        print("*")
