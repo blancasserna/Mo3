205ac0ea4cffd77e20a09ef0f84c832a983d3006
blancasserna <44443477+blancasserna@users.noreply.github.com>
2018-11-27 13:43:47 +0100

    Update division_exacte.py

--- python/division_exacte.py
+++ python/division_exacte.py
@@ -1,1 +1,10 @@
-
+#coding: utf-8
+divident = int(input("Indica un nombre:"))
+divisor = int(input("Indica un segon nombre:"))
+quocient = (divident/divisor)
+residu = quocient-abs(int(quocient))
+if (residu == 0 ):
+    print("La divisió és exacte.")
+else: 
+    if (residu != 0 ):
+        print("La divisió no és exacte.")
