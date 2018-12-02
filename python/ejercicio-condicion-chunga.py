ea8ffede274a2fd84056811d944f3c2ec3bda52a
blancasserna <44443477+blancasserna@users.noreply.github.com>
2018-12-02 13:08:12 +0100

    Update ejercicio-condicion-chunga.py

--- python/ejercicio-condicion-chunga.py
+++ python/ejercicio-condicion-chunga.py
@@ -1,1 +1,6 @@
-
+#coding: utf-8
+num_teclat = float(input(""))
+if ((num_teclat % 2 == 0) and (num_teclat > -11) and (num_teclat < 0)):
+    print(num_teclat)
+else:
+    print("No es pot llegir.")
