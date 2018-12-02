4cb2220b7691d375a21c04d6b1cdfac1c66a7576
blancasserna <44443477+blancasserna@users.noreply.github.com>
2018-12-02 12:38:43 +0100

    Create gasolina.py

---
+++ python/gasolina.py
@@ -0,0 +1,27 @@
+# coding:utf-8
+gasolina = input("Que gasolina quiere?")
+tipo = input("Indique el tipo que quiere:")
+litros = int(input("Indique cuantos litros quiere:"))
+if (gasolina == "super" and tipo == "normal"):
+    tipo_1 = litros * 1.5
+    print("En total seran" ,tipo_1)
+else:
+    if (gasolina == "super" and tipo == "turbo"):
+        tipo_2 = litros * 1.55
+        print("En total seran" ,tipo_2)
+    else:
+        if (gasolina == "sense plom" and tipo == "normal"):
+            tipo_3 = litros * 1.6
+            print("En total seran" ,tipo_3)
+        else:
+            if (gasolina == "sense plom" and tipo == "amb aditius"):
+                tipo_4 = litros * 1.65
+                print("En total seran" ,tipo_4)
+            else:
+                if (gasolina == "diesel" and tipo == "normal"):
+                    tipo_5 = litros * 1.7
+                    print("En total seran" ,tipo_5)
+                else:
+                    if (gasolina == "diesel" and tipo == "fast&furius"):
+                        tipo_6 = litros * 1.75
+                        print("En total seran" ,tipo_6)
