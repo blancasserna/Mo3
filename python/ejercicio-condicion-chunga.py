4a027033bf109f1f71e76ad477fcc9348b4d2eff
blancasserna <44443477+blancasserna@users.noreply.github.com>
2018-12-03 15:51:04 +0100

    Update ejercicio-condicion-chunga.py

--- python/ejercicio-condicion-chunga.py
+++ python/ejercicio-condicion-chunga.py
@@ -1,6 +1,18 @@
 #coding: utf-8
-num_teclat = float(input(""))
+num_teclat = int(input(""))
 if ((num_teclat % 2 == 0) and (num_teclat > -11) and (num_teclat < 0)):
     print(num_teclat)
 else:
     print("No es pot llegir.")
+
+    
+#coding: utf-8
+num_teclat = int(input("Escriu un número del teclat:"))
+if (num_teclat % 2 == 0):
+    print(,num_teclat, "és un nombre par")
+else:
+    if (num_teclat > -10 and num_teclat < 40):
+        print(,num_teclat, "està entre el -10 i el 40")
+    else:
+        if (num_teclat < 0):
+            print(,num_teclat, "és negatiu")
